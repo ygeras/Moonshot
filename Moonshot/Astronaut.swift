a110ef1a60530fef249269a605db4178f957a857
ygeras <86482316+Yus312@users.noreply.github.com>
2022-05-27 14:08:13 +0600

    Initial setup for the app

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Yuri Gerasimchuk on 27.05.2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
